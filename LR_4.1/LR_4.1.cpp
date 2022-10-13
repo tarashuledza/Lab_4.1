cd8e88bb214eab2ca963ef106d9bd4040c1921e0
Taras <tarashuledza14@gmail.com>
2022-10-13 18:11:37 +0300

    lab 4.1

---
+++ LR_4.1/LR_4.1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double k, N;
+	double P = 1;
+
+	cout << "k = "; cin >> k;
+	cout << "N = "; cin >> N;
+
+	k = N;
+	//1 спосіб While
+	while (k <= 19)
+	{
+		P *= ((k - N) / (k + N)) + 1;
+		k++;
+	}
+	cout << "1) P = " << P << endl;
+	//2 спосіб Do While
+	P = 1;
+	k = N;
+	do
+	{
+		P *= ((k - N) / (k + N)) + 1;
+		k++;
+	} while (k <= 19);
+
+	cout << "2) P = " << P << endl;
+
+	//3 спосіб for ++
+	P = 1;
+
+	for (k = N; k <= 19; k++)
+	{
+		P *= ((k - N) / (k + N)) + 1;
+	}
+	
+	cout << "3) P = " << P << endl;
+
+	
+
+	//4 спосіб for --
+	P = 1;
+	for (k = 19; k >= N; k--)
+	{
+		P *= ((k - N) / (k + N)) + 1;
+	}
+
+	cout << "4) P = " << P << endl;
+	
+	cin.get();
+	return 0;
+}
